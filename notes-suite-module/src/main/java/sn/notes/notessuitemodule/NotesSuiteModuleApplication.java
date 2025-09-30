5b0216b02defbf7e902b476da79d719d08069fdd
kuni77 <amadoudiao72@gmail.com>
2025-09-30 22:39:29 +0000

    feat: add app entry

---
+++ notes-suite-module/src/main/java/sn/notes/notessuitemodule/NotesSuiteModuleApplication.java
@@ -0,0 +1,13 @@
+package sn.notes.notessuitemodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesSuiteModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotesSuiteModuleApplication.class, args);
+    }
+
+}
---
+++ notes-suite-module/src/test/java/sn/notes/notessuitemodule/NotesSuiteModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.notes.notessuitemodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesSuiteModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
