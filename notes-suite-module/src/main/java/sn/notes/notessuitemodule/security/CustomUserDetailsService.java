6038fb39d7077a082972a0f00a19ef51fbd8b617
kuni77 <amadoudiao72@gmail.com>
2025-09-30 22:36:52 +0000

    feat: add app security

---
+++ notes-suite-module/src/main/java/sn/notes/notessuitemodule/security/CustomUserDetailsService.java
@@ -0,0 +1,31 @@
+package sn.notes.notessuitemodule.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import sn.notes.notessuitemodule.domain.User;
+import sn.notes.notessuitemodule.repository.UserRepository;
+
+import java.util.ArrayList;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPasswordHash(),
+                new ArrayList<>()
+        );
+    }
+}
---
+++ notes-suite-module/src/main/java/sn/notes/notessuitemodule/security/filters/JwtAuthEntryPoint.java
@@ -0,0 +1,35 @@
+package sn.notes.notessuitemodule.security.filters;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@Component
+public class JwtAuthEntryPoint implements AuthenticationEntryPoint {
+    private static final Logger logger = LoggerFactory.getLogger(JwtAuthEntryPoint.class);
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        logger.error("Unauthorized error: {}", authException.getMessage());
+        response.setContentType(APPLICATION_JSON_VALUE);
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        OutputStream outputStream = response.getOutputStream();
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(outputStream, Response
+                .accessDenied()
+                .setMessage("You do not have permission to access this resource.")
+                .setErrors("You do not have permission to access this resource."));
+        outputStream.flush();
+
+    }
+}
