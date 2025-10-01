2de297436a0e97d1ab38200bb06e41f80754616c
kuni77 <amadoudiao72@gmail.com>
2025-10-01 10:46:36 +0000

    feat: add api versionning config

---
+++ notes-suite-module/src/main/java/sn/notes/notessuitemodule/config/ApiVersioningConfig.java
@@ -0,0 +1,15 @@
+package sn.notes.notessuitemodule.config;
+
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class ApiVersioningConfig implements WebMvcConfigurer {
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        // Configuration pour supporter le versioning
+        configurer.addPathPrefix("/api/v1",
+                c -> c.getPackageName().contains("web.rest.v1"));
+        configurer.addPathPrefix("/api/v2",
+                c -> c.getPackageName().contains("web.rest.v2"));
+    }
+}
