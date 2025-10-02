e9b47612e93c89d3ba1f0e17fdc98ac23fcb6276
kuni77 <amadoudiao72@gmail.com>
2025-10-02 22:10:12 +0000

    fix: add @Configuration in App Versionning

--- notes-suite-module/src/main/java/sn/notes/notessuitemodule/config/ApiVersioningConfig.java
+++ notes-suite-module/src/main/java/sn/notes/notessuitemodule/config/ApiVersioningConfig.java
@@ -1,8 +1,10 @@
 package sn.notes.notessuitemodule.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class ApiVersioningConfig implements WebMvcConfigurer {
     @Override
     public void configurePathMatch(PathMatchConfigurer configurer) {
